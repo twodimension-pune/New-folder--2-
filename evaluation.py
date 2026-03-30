b0f0b209f4b54878a3d1340968a8dd64c3359379
Avinash <Avinashchauhan@twodimension.in>
2026-03-30 19:03:45 +0530

    add comment

--- evaluation.py
+++ evaluation.py
@@ -11,5 +11,7 @@ def generate_pdf_report(name, rounds, result, tech_score, hr_score=None):
     pdf.cell(200, 10, txt=f"Technical Score: {tech_score}/50", ln=1)
     if hr_score:
         pdf.cell(200, 10, txt=f"HR Score: {hr_score}/30", ln=1)
+        
+        # output is in pdf format
 
     pdf.output(f"feedback_reports/{name}_report.pdf")
